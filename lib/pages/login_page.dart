11cab5149509cad08eafdffc3c3da797a6a47405
Shaqal <shaqal7@gmail.com>
2020-06-02 16:20:23 +0200

    Login Page UI
    Part 1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,11 +9,98 @@ class LoginPage extends StatefulWidget{
 }
 
 class _LoginPageState extends State<LoginPage>{
+  double _deviceHeight;
+  double _deviceWidth;
+
+  GlobalKey<FormState> _formKey;
+
+  _LoginPageState(){
+    _formKey = GlobalKey<FormState>();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Align(child: Text("Login Page"),
+      backgroundColor: Theme.of(context).backgroundColor,
+        body: Align(
+          alignment: Alignment.center,
+            child: _loginPageUI()
+        )
+    );
+  }
+
+  Widget _loginPageUI(){
+    return Container(
+      height: _deviceHeight * 0.60,
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.10),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _headingWidget(),
+          _inputForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget _headingWidget(){
+    return Container(
+      height: _deviceHeight * 0.12,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Welcome back!",
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            "Please login to your account.",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w200),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _inputForm(){
+    return Container(
+      height: _deviceHeight * 0.16,
+      child: Form(
+        key: _formKey,
+        onChanged: (){},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _emailTextField(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _emailTextField(){
+    return TextFormField(
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {},
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "Email Address",
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+                color: Colors.white),
+        ),
+      ),
     );
   }
 }
