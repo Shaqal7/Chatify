9ce05f1bf29cb4404e29f0b22ec821c39a9dc719
Shaqal <shaqal7@gmail.com>
2020-06-19 12:37:56 +0200

    HomePage | Tab Bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'profile_page.dart';
 
 class HomePage extends StatefulWidget{
 
@@ -8,7 +9,13 @@ class HomePage extends StatefulWidget{
   }
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  _HomePageState(){
+    _tabController = TabController(length: 3, vsync: this, initialIndex: 1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +27,41 @@ class _HomePageState extends State<HomePage> {
           title: TextStyle(fontSize: 16),
         ),
         title: Text("Chatify"),
+        bottom: TabBar(
+          unselectedLabelColor: Colors.grey,
+          indicatorColor: Colors.blue,
+          labelColor: Colors.blue,
+          controller: _tabController,
+          tabs: [
+            Tab(
+              icon: Icon(
+                Icons.people_outline,
+                size: 24,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.chat_bubble_outline,
+                size: 24,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.person_outline,
+                size: 24,
+              ),
+            ),
+        ]),
       ),
+      body: _tabBarPages(),
     );
+  }
+
+  Widget _tabBarPages() {
+    return TabBarView(controller: _tabController, children: <Widget>[
+      ProfilePage(),
+      ProfilePage(),
+      ProfilePage(),
+    ]);
   }
 }
