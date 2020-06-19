8ba4924165230e391d9ff4b36e855a2ee6899dae
Shaqal <shaqal7@gmail.com>
2020-06-19 16:10:33 +0200

    Recent Conversations Page

--- lib/pages/recent_conversations_page.dart
+++ lib/pages/recent_conversations_page.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:provider/provider.dart';
+import '../providers/auth_provider.dart';
+import '../services/db_service.dart';
+import '../models/conversation.dart';
 
 class RecentConversationsPage extends StatelessWidget {
 
@@ -12,46 +19,62 @@ class RecentConversationsPage extends StatelessWidget {
     return Container(
       height: _height,
       width: _width,
-      child: _conversationsListViewWidget(),
-    );
-  }
-
-  Widget _conversationsListViewWidget(){
-    return Container(
-      height: _height,
-      width: _width,
-      child: ListView.builder(
-          itemCount: 1,
-        itemBuilder: (_context, _index) {
-            return ListTile(
-              onTap: () {},
-              title: Text("Testowy test"),
-              subtitle: Text("subtitle"),
-              leading: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  image: DecorationImage(
-                    image: NetworkImage('https://firebasestorage.googleapis.com/v0/b/chatify-cea06.appspot.com/o/profile_images%2FwsJ9m8D9w6OHuEjASM2vz8G47Zu1?alt=media&token=2d3fdec2-6270-4ee6-aea7-9a19d2a314fa'),
-                  )
-                ),
-              ),
-              trailing: _listTileTrailingWidget(),
-            );
-        },
+      child: ChangeNotifierProvider<AuthProvider>.value(
+        value: AuthProvider.instance,
+        child: _conversationsListViewWidget(),
       ),
     );
   }
 
-  Widget _listTileTrailingWidget(){
+  Widget _conversationsListViewWidget(){
+    return Builder(builder: (BuildContext _context){
+      var _auth = Provider.of<AuthProvider>(_context);
+      return Container(
+        height: _height,
+        width: _width,
+        child: StreamBuilder<List<ConversationSnippet>>(
+          stream: DBService.instance.getUserConversations(_auth.user.uid),
+            builder: (_context, _snapshot){
+            var _data = _snapshot.data;
+            return _snapshot.hasData ?
+          ListView.builder(
+            itemCount: _data.length,
+            itemBuilder: (_context, _index) {
+              return ListTile(
+                onTap: () {},
+                title: Text(_data[_index].name),
+                subtitle: Text(_data[_index].lastMessage),
+                leading: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(_data[_index].image),
+                      )
+                  ),
+                ),
+                trailing: _listTileTrailingWidget(_data[_index].timestamp),
+              );
+            },
+          ) : SpinKitWanderingCubes(
+              color: Colors.blue,
+              size: 50.0,
+              );
+        })
+      );
+    });
+  }
+
+  Widget _listTileTrailingWidget(Timestamp _lastMessageTimestamp){
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
         Text(
-          "lastSeen",
+          timeago.format(_lastMessageTimestamp.toDate()),
           style: TextStyle(
               fontSize: 15
           ),
--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/contact.dart';
+import '../models/conversation.dart';
 
 class DBService {
   static DBService instance = DBService();
@@ -11,6 +12,7 @@ class DBService {
   }
 
   String _userCollection = "Users";
+  String _conversationsCollection = "Conversations";
 
   Future<void> createUserInDB(
     String _uid, String _name, String _email, String _imageURL) async {
@@ -30,6 +32,15 @@ class DBService {
     var _ref = _db.collection(_userCollection).document(_userID);
     return _ref.get().asStream().map((_snapshot) {
       return Contact.fromFirestore(_snapshot);
+    });
+  }
+
+  Stream<List<ConversationSnippet>> getUserConversations(String _userID) {
+    var _ref = _db.collection(_userCollection).document(_userID).collection(_conversationsCollection);
+    return _ref.snapshots().map((_snapshot){
+      return _snapshot.documents.map((_doc) {
+        return ConversationSnippet.fromFirestore(_doc);
+      }).toList();
     });
   }
 }
